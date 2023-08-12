a2ee6341007c15423e8aced15388f9a964fd46dc
Pedro Samuel <cardev604@gmail.com>
2023-08-12 02:31:37 -0300

    feat: removing index.js file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { CaixaDaLanchonete } from "./caixa-da-lanchonete.js";
-
-const seila = new CaixaDaLanchonete()
-
-const array = seila.transformarArrayDeItens(['cafe,1', 'suco,2'])
-const calculo = seila.calculaPrecoParcial(array)
-const fds = seila.validaItens(array)
-console.log(calculo)
